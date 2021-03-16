a296fca63f0f968746a3d520d9f90df2b44c513c
Shriraj <shriraj.hegde@gmail.com>
2021-03-16 11:37:47 +0530

    Improve accuracy Considering only 1st quandrant for better accuracy

--- pi.f90
+++ pi.f90
@@ -10,7 +10,6 @@ program pi
         do i=1,m
             call random_number(x) ! generating random points in the square
             call random_number(y)
-            x = x*2 - 1 ; y = y*2 -1
             if ((x**2+y**2).le.1) then !check if point lies inside circle, and count if it does
                 in_circle=in_circle+1
             endif
--- pi.py
+++ pi.py
@@ -6,8 +6,8 @@ value=0.0
 for j in range(1,n+1):
     in_circle=0
     for i in range(1, m+1):
-        x = random.uniform(-1, 1) # generating random points in the square
-        y=random.uniform(-1,1)
+        x = random.uniform(0, 1) # generating random points in the square
+        y=random.uniform(0,1)
         if (x**2+y**2) <= 1:  # check if point lies inside circle, and count if it does
             in_circle+=1
     value += (in_circle/i) * 4  # taking average over multiple runs
